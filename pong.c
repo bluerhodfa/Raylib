98fa71be37c178bc7ec79dccc7f4c7c62e0280b2
bluerhodfa <ira.theobold@gmail.com>
2026-05-16 18:36:37 +0100

    rewrite with additional code from Colan Biemer

--- pong.c
+++ pong.c
@@ -1,143 +1,243 @@
 #include "raylib.h"
+#include "raymath.h"
 
-#define LIGHT_GREEN (Color){ 125, 204, 184,255 }
+// -------------------------------------------------------------
+// Simple pong game in Raylib and C
+//
+//  MIT License
+//  Copyright (c) 2026 Ira Theobold
+//
+// Based on https://github.com/bi3mer/raylib-pong
+// -------------------------------------------------------------
 #define LIGHT_BLUE (Color){ 102, 178, 255,255 }
 #define DARK_BLUE (Color){ 51, 153, 255,255 }
-#define DARK_GREEN (Color){ 20, 160, 133,255 }
-#define MY_GREEN (Color){ 28, 185 ,154,  255 }
+#define GAME_OVER "GAME OVER!"
 
-int player1Score = 0;
-int player2Score = 0;
-bool active = false;                // Game not running
-bool pause = false;                 // Pause control flag
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
 int main(void)
 {
-    InitWindow(1280, 800, "raylib retro gaming - pong");
+    // setup game state
 
     const int screenWidth = 1280;
     const int screenHeight = 800;
+    const int playStartHeight = 50;
 
-    int ballRadius = 20;
-    int ballSpeedX = 0; 
-    int ballSpeedY = 0; 
-    int ballPositionX = GetScreenWidth() / 2 ;
-    int ballPositionY = GetScreenHeight() / 2;
+
+    int player1Score = 0;
+    int player2Score = 0;
+
+    bool paused = false;
+    bool active = false;
+
+    // setup the player paddles
+    Rectangle playerLeftPaddle =
+    { 
+      .x = 0.05f * screenWidth,
+      .y = 0.40f * screenHeight,
+      .width = 0.01f * screenWidth,
+      .height = 0.2f * screenHeight,
+    };
+
+    Rectangle playerRightPaddle =
+    { 
+      .x = 0.95f * screenWidth,
+      .y = 0.40f * screenHeight,
+      .width = 0.01f * screenWidth,
+      .height = 0.2f * screenHeight,
+    };
+
+    Vector2 ballPosition = 
+    {
+        .x = 0.5f * screenWidth,
+        .y = 0.5f * screenHeight,
+    };
+
+    float ballRadius = screenHeight * 0.015f;
+
+    Vector2 ballSpeed = 
+    {
+        .x = -300.0f,
+        .y = 0.0f,
+    };
+
     
-    int player1PositionY = 400;
-    int player2PositionY = 400;
-
-// setuo the player paddle sizes
-    Rectangle player1Rect = { 20, player1PositionY, 8, 100 };
-    Rectangle player2Rect = { screenWidth - 20, player2PositionY, 8, 100 };
-// Sound
-    InitAudioDevice();
-    Sound ping = LoadSound("resources/ping.mp3");
-    // init ball speed
-    ballSpeedX = GetRandomValue(5,-4);
-    ballSpeedY = GetRandomValue(6,-3);
-
+    // Run the game
+    InitWindow(screenWidth , screenHeight, "raylib pong");
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
 
-        if (IsKeyPressed(KEY_P)) pause = !pause;
+        if (IsKeyPressed(KEY_P))
+        {
+            paused = !paused;
+        }
 
+        if (!paused)
+        {
+            // Handle key presses
+            const float paddleMod = 10.0f;
+            //player 1
+            if (IsKeyDown(KEY_W)) {
+                playerLeftPaddle.y = MAX(playStartHeight, playerLeftPaddle.y - paddleMod);
+            }
+            if (IsKeyDown(KEY_S))
+            {
+                playerLeftPaddle.y = MIN(screenHeight - playerLeftPaddle.height, playerLeftPaddle.y + paddleMod);
+            }
+
+            if (IsKeyDown(KEY_UP))
+            {
+                playerRightPaddle.y = MAX(playStartHeight, playerRightPaddle.y - paddleMod);
+            }
+
+            if (IsKeyDown(KEY_DOWN))
+            {
+                playerRightPaddle.y =
+                    MIN(screenHeight - playerRightPaddle.height, playerRightPaddle.y + paddleMod);
+            }
+
+            const float dt = GetFrameTime();
+
+            ballPosition.x += ballSpeed.x * dt;
+            ballPosition.y += ballSpeed.y * dt;
+
+           // handle collisions
+            if (ballSpeed.x < 0)
+            {
+                if (CheckCollisionCircleRec(ballPosition, ballRadius, playerLeftPaddle))
+                {
+                    float hitPos = (ballPosition.y - playerLeftPaddle.y) / playerLeftPaddle.height;
+                    hitPos = Clamp(hitPos, 0.0f, 1.0f);
+
+                    const float a = (hitPos - 0.5f) * PI / 4;
+                    const float speed = Vector2Length(ballSpeed) * 1.05f;
+                    ballSpeed.x = speed * cosf(a);
+                    ballSpeed.y = speed * sinf(a);
+                }
+            }
+            else
+            {
+                if (CheckCollisionCircleRec(ballPosition, ballRadius, playerRightPaddle))
+                {
+                    float hitPos = (ballPosition.y - playerRightPaddle.y) / playerRightPaddle.height;
+                    hitPos = Clamp(hitPos, 0.0f, 1.0f);
+
+                    const float a = PI - (hitPos - 0.5f) * PI / 4;
+                    const float speed = Vector2Length(ballSpeed) * 1.05f;
+                    ballSpeed.x = speed * cosf(a);
+                    ballSpeed.y = speed * sinf(a);
+                }
+            }
+
+            // collision with top and bottom wall
+            if (ballSpeed.y > 0)
+            {
+                if (ballPosition.y >= screenHeight - ballRadius)
+                {
+                    ballSpeed.y *= -1;
+                    ballPosition.y = screenHeight - ballRadius;
+                }
+            }
+            else
+            {
+                if (ballPosition.y <= playStartHeight + ballRadius)
+                {
+                    ballSpeed.y *= -1;
+                    ballPosition.y = playStartHeight + ballRadius;
+                }
+            }
+
+            // handle scoring
+            if (ballPosition.x < 0)
+            {
+                ++player2Score;
+
+                ballSpeed.x = -300.0f;
+                ballSpeed.y = 0.0f;
+                ballPosition.x = 0.5f * screenWidth;
+                ballPosition.y = 0.5f * screenHeight;
+            }
+            else if (ballPosition.x > screenWidth)
+            {
+                ++player1Score;
+
+                ballSpeed.x = 300.0f;
+                ballSpeed.y = 0.0f;
+                ballPosition.x = 0.5f * screenWidth;
+                ballPosition.y = 0.5f * screenHeight;
+            }
+        }
+
+        // Render the game screen
         BeginDrawing();
+
             ClearBackground(DARK_BLUE);
-            // DrawText(TextFormat("X: %d Y: %d", ballPositionX, ballPositionY) , screenWidth / 2 , 150, 50, WHITE);
 
             DrawRectangle(screenWidth / 2, 0, screenWidth / 2, screenHeight, LIGHT_BLUE);
-//          void DrawCircle(int centerX, int centerY, float radius, Color color);  // Draw a color-filled circle  
-            DrawCircle(screenWidth / 2, screenHeight / 2, 250  / 2, BLUE);
-            DrawCircleLines(screenWidth / 2, screenHeight / 2, 250  / 2, WHITE);
-            DrawLine(GetScreenWidth() / 2, 0, GetScreenWidth() / 2, GetScreenHeight(), WHITE);
+            DrawCircle(screenWidth / 2, screenHeight / 2, 150  / 2, BLUE);
+            DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
+
+            DrawRectangleRec(playerLeftPaddle, WHITE);
+            DrawRectangleRec(playerRightPaddle, WHITE);
+            DrawCircleV(ballPosition, ballRadius, YELLOW);
 
             DrawText(TextFormat("%i", player1Score), screenWidth / 4 - 20, 20, 80, WHITE);
             DrawText(TextFormat("%i", player2Score), 3 * screenWidth / 4, 20, 80, WHITE);
 
-            if (active == false) {
-                DrawText("Press SPACE BAR to start", 490 , 50, 25, WHITE);
-                DrawText("Player 1 move with UP/DOWN keys, Player 2 move withi A/Z keys. ", 250 , 120, 25, WHITE);
-                DrawText("Press P to pause/Resart", 490 , 150, 25, WHITE);
-            }
-            //player 1
-            if (IsKeyDown(KEY_UP)) {
-                player2Rect.y -= 7;
-                if (player2Rect.y <= 0 ) player2Rect.y = 0;
-            }
+            // Start of game instructions
+            if (active == false)
+            {
+                // pause at start
 
-            if (IsKeyDown(KEY_DOWN)) {
-                player2Rect.y += 7;
-                if (player2Rect.y >=  700  ) player2Rect.y = 700;
-            }
-            // Player 2
-            if (IsKeyDown(KEY_A)) {
-                player1Rect.y -= 7;
-                if (player1Rect.y <= 0 ) player1Rect.y = 0;
-            }
+                paused = true;
 
-            if (IsKeyDown(KEY_Z)) {
-                player1Rect.y += 7;
-                if (player1Rect.y >=  700  ) player1Rect.y = 700;
-            }
+                const int startFontSize = 50;
+                const char *startText = "Press ENTER to start game";
+                const char *keysText = "Player controls: W/S and UP/DOWN keys";
+                const int startTextWidth = MeasureText(startText, startFontSize);
+                const int keysTextWidth = MeasureText(keysText, startFontSize);
+                const int sX = (screenWidth - startTextWidth) / 2;
+                const int kX = (screenWidth - keysTextWidth) / 2;
 
-            if (IsKeyPressed(KEY_SPACE)) {
-                active = true;
-                ballPositionX += ballSpeedX;
-                ballPositionY += ballSpeedY;
-            }
+                DrawText(startText, sX, screenHeight / 4 - 20, startFontSize, WHITE);
+                DrawText(keysText, kX, screenHeight / 4 + startFontSize, startFontSize, WHITE);
 
-            if (active == true && !pause) {
-                ballPositionX += ballSpeedX;
-                ballPositionY += ballSpeedY;
+                    if(IsKeyDown(KEY_ENTER))
+                    {
+                        active = true;
+                        paused = false;
 
-
-                    if (ballPositionX >= GetScreenWidth()) {
-                        ballPositionX = GetScreenWidth() / 2;
-                        ballPositionY = GetScreenHeight() / 2;
-                        player1Score += 1;
-                        active = false;
                     }
-
-                    if (ballPositionX <= 0) {
-                        ballPositionX = GetScreenWidth() / 2;
-                        ballPositionY = GetScreenHeight() / 2;
-                        player2Score += 1;
-                        active = false;
-                    } 
-                // bool CheckCollisionCircleRec(Vector2 center, float radius, Rectangle rec); // Check collision between circle and rectangle
-                // Check collision the ball and the paddles
-                if (CheckCollisionCircleRec((Vector2) { ballPositionX , ballPositionY  }, 20.0f, player1Rect ) == true ) {
-                    PlaySound(ping);
-                    ballSpeedX = 4; 
-                    ballPositionX += ballSpeedX;
-                }
-
-                if (CheckCollisionCircleRec((Vector2) { ballPositionX , ballPositionY  }, 20.0f, player2Rect ) == true ) {
-                    PlaySound(ping);
-                    ballSpeedX = -4; 
-                    ballPositionX += ballSpeedX;
-                }
-
             }
-            if (ballPositionY <= 0 + ballRadius / 2) ballSpeedY = 6;
-            if (ballPositionY >= GetScreenHeight() - ballRadius / 2) ballSpeedY = -6;
 
-            DrawCircle( ballPositionX + ballSpeedX, ballPositionY + ballSpeedY,  ballRadius, YELLOW);
-            // void DrawRectangleRounded(Rectangle rec, float roundness, int segments, Color color); // Draw rectangle with rounded edges
-            DrawRectangleRounded(player1Rect, 2.0f, 5, WHITE);
-            DrawRectangleRounded(player2Rect, 2.0f, 5, WHITE);
+           // render pause, if necessary
+           if (paused)
+           { 
+            const int pausedFontSize = 80;
+            const char *pauseText = "Paused";
+            const int pauseWidth = MeasureText(pauseText, pausedFontSize);
+            const int pX = (screenWidth - pauseWidth) / 2;
+
+            const Rectangle rec = {
+                .x = pX - 10,
+                .y = screenHeight / 2 - 30,
+                .width = pauseWidth + 20,
+                .height = 100,
+            };
+
+            DrawRectangleRec(rec, WHITE);
+            DrawText(pauseText, pX, screenHeight / 2 - 20, pausedFontSize, BLACK);
+            }  
+
 
         EndDrawing();
     }
 
     CloseWindow();
-
-    UnloadSound(ping);
-
     return 0;
 }
 
